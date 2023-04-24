evergreenbear/android_hardware_oplus

hardware: OPlusExtras 7.6 import
- Now using a better resource based configuration method to allow
  universally any Oplus device to ship this package and configure it
  by using overlays in their device specific tree.

Co-authored-by: Max Weninger <max.weninger@gmail.com>
Co-authored-by: oddlyspaced <oddlyspaced1@gmail.com>
Co-authored-by: TheScarastic <warabhishek@gmail.com>
Co-authored-by: LuK1337 <priv.luk@gmail.com>
Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Co-authored-by: lostark13 <tushar9915@gmail.com>
Co-authored-by: cristianvaz <cristianvaz81993@gmail.com>
Co-authored-by: jhenrique09 <jhsv09@gmail.com>
Co-authored-by: AliHasan7671 <AHB7671@gmail.com>
Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>
Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
Co-authored-by: darkobas <darkobas@gmail.com>
Co-authored-by: Vachounet <vachounet@live.fr>
Co-authored-by: kldoc <fred.berbigier@gmail.com>
Co-authored-bt: Nauval Rizky <enuma.alrizky@gmail.com>
Co-authored-by: Christian Oder <myself5@carbonrom.org>
Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
Co-authored-by: SiddharthBharadwaj <siddharthbharadwaj45@gmail.com>
Co-authored-by: Michele Bono <bono.michele94@gmail.com>
Co-authored-by: LibXZR <xzr467706992@163.com>
Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
Co-authored-by: pri0818 <priyanshusinghal0818@gmail.com>
Co-authored-by: Singh <sonal.singh.19993@gmail.com>
Co-authored-by: Joey Huab <joey@evolution-x.org>
Signed-off-by: AnierinB <anierin@evolution-x.org>

OPlusExtras/src/org/evolution/oplus/OPlusExtras/ModeSwitch/PCCModeSwitch.java
@@ -1,0 +1,45 @@
+/*
+ * Copyright (C) 2016 The OmniROM Project
+ *               2023 The Evolution X Project
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+package org.evolution.oplus.OPlusExtras.modeswitch;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
+import org.evolution.oplus.OPlusExtras.FileUtils;
+import org.evolution.oplus.OPlusExtras.R;
+
+public class PCCModeSwitch implements OnPreferenceChangeListener {
+
+    private static final int NODE = R.string.node_pcc_mode_switch;
+
+    public static String getFile(Context context) {
+        String file = context.getResources().getString(NODE);
+        if (FileUtils.fileWritable(file)) {
+            return file;
+        }
+        return null;
+    }
+
+    public static boolean isSupported(Context context) {
+        return FileUtils.fileWritable(getFile(context));
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return FileUtils.getFileValueAsBoolean(getFile(context), false,
+            context.getResources().getString(R.string.node_pcc_mode_switch_true),
+            context.getResources().getString(R.string.node_pcc_mode_switch_false));
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        FileUtils.writeValue(getFile(preference.getContext()), enabled ? "1" : "0");
+        return true;
+    }
+}

OPlusExtras/src/org/evolution/oplus/OPlusExtras/Startup.java
@@ -1,0 +1,119 @@
+/*
+ * Copyright (C) 2016 The OmniROM Project
+ *               2022 The Evolution X Project
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+package org.evolution.oplus.OPlusExtras;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.provider.Settings;
+import androidx.preference.PreferenceManager;
+
+import org.evolution.oplus.OPlusExtras.colorspace.ColorSpaceUtils;
+import org.evolution.oplus.OPlusExtras.doze.DozeUtils;
+import org.evolution.oplus.OPlusExtras.FileUtils;
+import org.evolution.oplus.OPlusExtras.modeswitch.*;
+import org.evolution.oplus.OPlusExtras.preferences.*;
+import org.evolution.oplus.OPlusExtras.refreshrate.RefreshUtils;
+import org.evolution.oplus.OPlusExtras.services.FPSInfoService;
+import org.evolution.oplus.OPlusExtras.touch.TouchscreenGestureSettings;
+
+public class Startup extends BroadcastReceiver {
+
+    private static final String TAG = "BootReceiver";
+
+    @Override
+    public void onReceive(final Context context, final Intent bootintent) {
+
+        AdrenoBoostPreference.restore(context);
+        BluePreference.restore(context);
+        ContrastPreference.restore(context);
+        ColorSpaceUtils.startService(context);
+        DozeUtils.checkDozeService(context);
+        GreenPreference.restore(context);
+        HuePreference.restore(context);
+        MaxBrightnessPreference.restore(context);
+        OPlusExtras.restoreSliderStates(context);
+        RedPreference.restore(context);
+        RefreshUtils.startService(context);
+        SaturationPreference.restore(context);
+        TouchscreenGestureSettings.MainSettingsFragment.restoreTouchscreenGestureStates(context);
+        ValuePreference.restore(context);
+        VibratorStrengthPreference.restore(context);
+
+        boolean enabled = false;
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        enabled = sharedPrefs.getBoolean(OPlusExtras.KEY_DC_SWITCH, false);
+        if (enabled) {
+        restore(DCModeSwitch.getFile(context), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(OPlusExtras.KEY_HBM_SWITCH, false);
+        if (enabled) {
+        restore(HBMModeSwitch.getFile(context), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(OPlusExtras.KEY_PCC_SWITCH, false);
+        if (enabled) {
+        restore(PCCModeSwitch.getFile(context), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(OPlusExtras.KEY_FPS_INFO, false);
+        if (enabled) {
+            context.startService(new Intent(context, FPSInfoService.class));
+               }
+        enabled = sharedPrefs.getBoolean(OPlusExtras.KEY_FSYNC_SWITCH, false);
+        if (enabled) {
+        restore(FSyncModeSwitch.getFile(context), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(OPlusExtras.KEY_POWERSHARE_SWITCH, false);
+        if (enabled) {
+            restore(PowerShareModeSwitch.getFile(context), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(OPlusExtras.KEY_QUIET_MODE_SWITCH, false);
+        if (enabled) {
+            restore(QuietModeSwitch.getFile(context), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(OPlusExtras.KEY_GAME_SWITCH, false);
+        if (enabled) {
+            restore(GameModeSwitch.getFile(context), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(OPlusExtras.KEY_TP_EDGE_LIMIT_SWITCH, false);
+        if (enabled) {
+            restore(TPEdgeLimitModeSwitch.getFile(context), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(OPlusExtras.KEY_POWER_EFFICIENT_WQ_SWITCH, false);
+        if (enabled) {
+            restore(PowerEfficientWorkqueueModeSwitch.getFile(context), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(OPlusExtras.KEY_TOUCHBOOST_SWITCH, false);
+        if (enabled) {
+            restore(TouchBoostModeSwitch.getFile(context), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(OPlusExtras.KEY_OTG_SWITCH, false);
+        if (enabled) {
+            restore(OTGModeSwitch.getFile(context), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(OPlusExtras.KEY_USB2_SWITCH, false);
+        if (enabled) {
+        restore(USB2FastChargeModeSwitch.getFile(context), enabled);
+        }
+    }
+
+    private void restore(String file, boolean enabled) {
+        if (file == null) {
+            return;
+        }
+        if (enabled) {
+            FileUtils.writeValue(file, "1");
+        }
+    }
+
+    private void restore(String file, String value) {
+        if (file == null) {
+            return;
+        }
+        FileUtils.writeValue(file, value);
+    }
+}

manidweep/hardware-oplus

OPlusExtras/src/org/custom/oplus/OPlusExtras/ModeSwitch/PCCModeSwitch.java
@@ -1,0 +1,45 @@
+/*
+ * Copyright (C) 2016 The OmniROM Project
+ *               2023 The Evolution X Project
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+package org.custom.oplus.OPlusExtras.modeswitch;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
+import org.custom.oplus.OPlusExtras.FileUtils;
+import org.custom.oplus.OPlusExtras.R;
+
+public class PCCModeSwitch implements OnPreferenceChangeListener {
+
+    private static final int NODE = R.string.node_pcc_mode_switch;
+
+    public static String getFile(Context context) {
+        String file = context.getResources().getString(NODE);
+        if (FileUtils.fileWritable(file)) {
+            return file;
+        }
+        return null;
+    }
+
+    public static boolean isSupported(Context context) {
+        return FileUtils.fileWritable(getFile(context));
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return FileUtils.getFileValueAsBoolean(getFile(context), false,
+            context.getResources().getString(R.string.node_pcc_mode_switch_true),
+            context.getResources().getString(R.string.node_pcc_mode_switch_false));
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        FileUtils.writeValue(getFile(preference.getContext()), enabled ? "1" : "0");
+        return true;
+    }
+}

OPlusExtras/src/org/custom/oplus/OPlusExtras/Startup.java
@@ -1,0 +1,119 @@
+/*
+ * Copyright (C) 2016 The OmniROM Project
+ *               2022 The Evolution X Project
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+package org.custom.oplus.OPlusExtras;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.provider.Settings;
+import androidx.preference.PreferenceManager;
+
+import org.custom.oplus.OPlusExtras.colorspace.ColorSpaceUtils;
+import org.custom.oplus.OPlusExtras.doze.DozeUtils;
+import org.custom.oplus.OPlusExtras.FileUtils;
+import org.custom.oplus.OPlusExtras.modeswitch.*;
+import org.custom.oplus.OPlusExtras.preferences.*;
+import org.custom.oplus.OPlusExtras.refreshrate.RefreshUtils;
+import org.custom.oplus.OPlusExtras.services.FPSInfoService;
+import org.custom.oplus.OPlusExtras.touch.TouchscreenGestureSettings;
+
+public class Startup extends BroadcastReceiver {
+
+    private static final String TAG = "BootReceiver";
+
+    @Override
+    public void onReceive(final Context context, final Intent bootintent) {
+
+        AdrenoBoostPreference.restore(context);
+        BluePreference.restore(context);
+        ContrastPreference.restore(context);
+        ColorSpaceUtils.startService(context);
+        DozeUtils.checkDozeService(context);
+        GreenPreference.restore(context);
+        HuePreference.restore(context);
+        MaxBrightnessPreference.restore(context);
+        OPlusExtras.restoreSliderStates(context);
+        RedPreference.restore(context);
+        RefreshUtils.startService(context);
+        SaturationPreference.restore(context);
+        TouchscreenGestureSettings.MainSettingsFragment.restoreTouchscreenGestureStates(context);
+        ValuePreference.restore(context);
+        VibratorStrengthPreference.restore(context);
+
+        boolean enabled = false;
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        enabled = sharedPrefs.getBoolean(OPlusExtras.KEY_DC_SWITCH, false);
+        if (enabled) {
+        restore(DCModeSwitch.getFile(context), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(OPlusExtras.KEY_HBM_SWITCH, false);
+        if (enabled) {
+        restore(HBMModeSwitch.getFile(context), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(OPlusExtras.KEY_PCC_SWITCH, false);
+        if (enabled) {
+        restore(PCCModeSwitch.getFile(context), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(OPlusExtras.KEY_FPS_INFO, false);
+        if (enabled) {
+            context.startService(new Intent(context, FPSInfoService.class));
+               }
+        enabled = sharedPrefs.getBoolean(OPlusExtras.KEY_FSYNC_SWITCH, false);
+        if (enabled) {
+        restore(FSyncModeSwitch.getFile(context), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(OPlusExtras.KEY_POWERSHARE_SWITCH, false);
+        if (enabled) {
+            restore(PowerShareModeSwitch.getFile(context), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(OPlusExtras.KEY_QUIET_MODE_SWITCH, false);
+        if (enabled) {
+            restore(QuietModeSwitch.getFile(context), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(OPlusExtras.KEY_GAME_SWITCH, false);
+        if (enabled) {
+            restore(GameModeSwitch.getFile(context), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(OPlusExtras.KEY_TP_EDGE_LIMIT_SWITCH, false);
+        if (enabled) {
+            restore(TPEdgeLimitModeSwitch.getFile(context), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(OPlusExtras.KEY_POWER_EFFICIENT_WQ_SWITCH, false);
+        if (enabled) {
+            restore(PowerEfficientWorkqueueModeSwitch.getFile(context), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(OPlusExtras.KEY_TOUCHBOOST_SWITCH, false);
+        if (enabled) {
+            restore(TouchBoostModeSwitch.getFile(context), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(OPlusExtras.KEY_OTG_SWITCH, false);
+        if (enabled) {
+            restore(OTGModeSwitch.getFile(context), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(OPlusExtras.KEY_USB2_SWITCH, false);
+        if (enabled) {
+        restore(USB2FastChargeModeSwitch.getFile(context), enabled);
+        }
+    }
+
+    private void restore(String file, boolean enabled) {
+        if (file == null) {
+            return;
+        }
+        if (enabled) {
+            FileUtils.writeValue(file, "1");
+        }
+    }
+
+    private void restore(String file, String value) {
+        if (file == null) {
+            return;
+        }
+        FileUtils.writeValue(file, value);
+    }
+}